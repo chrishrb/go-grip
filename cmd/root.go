9ff1fbfc8d8423f9b997e0cce44afc79963c6f50
Christoph Herb <52382992+chrishrb@users.noreply.github.com>
2024-12-23 16:07:06 +0100

    21 Add option to remove bounding box

--- cmd/root.go
+++ cmd/root.go
@@ -17,8 +17,16 @@ var rootCmd = &cobra.Command{
 		openReadme, _ := cmd.Flags().GetBool("readme")
 		host, _ := cmd.Flags().GetString("host")
 		port, _ := cmd.Flags().GetInt("port")
+    boundingBox, _ := cmd.Flags().GetBool("bounding-box")
 
-		client := pkg.Client{Dark: dark, OpenBrowser: browser, Host: host, Port: port, OpenReadme: openReadme}
+    client := pkg.Client{
+      Dark: dark,
+      OpenBrowser: browser,
+      Host: host,
+      Port: port,
+      OpenReadme: openReadme,
+      BoundingBox: boundingBox,
+    }
 
 		var file string
 		if len(args) == 1 {
@@ -42,4 +50,5 @@ func init() {
 	rootCmd.Flags().BoolP("readme", "r", true, "Open readme if no file provided")
 	rootCmd.Flags().StringP("host", "H", "localhost", "Host to use")
 	rootCmd.Flags().IntP("port", "p", 6419, "Port to use")
+	rootCmd.Flags().BoolP("bounding-box", "B", true, "Add bounding box to HTML")
 }
