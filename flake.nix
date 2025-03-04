deb857f70d6f2ff2ca5cf52d55b2eb5e40f2e916
Gustavo "Guz" L de Mello <contact@guz.one>
2025-03-04 14:46:48 -0300

    remove the necessity of flake-utils

--- flake.nix
+++ flake.nix
@@ -2,25 +2,35 @@
   description = "go-grip - render your markdown files local";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, ... }@inputs: inputs.flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        go-grip = pkgs.buildGoModule {
-          name = "go-grip";
-          src = self;
-          # Only for updating vendorHas
-          # vendorHash = "sha256-RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR=";
-          vendorHash = "sha256-aU6vo/uqJzctD7Q8HPFzHXVVJwMmlzQXhAA6LSkRAow=";
-        };
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }: let
+    systems = [
+      "aarch64-linux"
+      "aarch64-darwin"
+      "x86_64-darwin"
+      "x86_64-linux"
+    ];
+    forAllSystems = f:
+      nixpkgs.lib.genAttrs systems (system: let
+        pkgs = import nixpkgs {inherit system;};
       in
-      {
-        packages.default = go-grip;
-        devShells.default = pkgs.mkShell {
-          nativeBuildInputs = [ go-grip ];
-        };
-      }
-    );
+        f pkgs);
+  in {
+    packages = forAllSystems (pkgs: {
+      default = pkgs.buildGoModule {
+        name = "go-grip";
+        src = self;
+        # Only for updating vendorHas
+        # vendorHash = "sha256-RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR=";
+        vendorHash = "sha256-aU6vo/uqJzctD7Q8HPFzHXVVJwMmlzQXhAA6LSkRAow=";
+      };
+    });
+    devShells = forAllSystems (pkgs: {
+      default = self.packages.${pkgs.system}.default;
+    });
+  };
 }
