0a2288536f7f29388da25b66dcbdfafd2da2c697
prime-run <prime0run@gmail.com>
2025-05-27 00:21:24 +0000

    feat: ver command || base for refactoring

--- cmd/version.go
+++ cmd/version.go
@@ -6,9 +6,10 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const (
-	Version   = "0.1.0"
-	BuildDate = "2023-07-15"
+var (
+	Version    = "dev"
+	BuildDate  = "unknown"
+	CommitHash = "unknown"
 )
 
 var versionCmd = &cobra.Command{
@@ -16,7 +17,7 @@ var versionCmd = &cobra.Command{
 	Short: "Print the version number of go-grip",
 	Long:  `Display the version information for go-grip.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("v%s (built: %s)\n", "ver", "build")
+		fmt.Printf("v%s (commit: %s, built: %s)\n", Version, CommitHash, BuildDate)
 	},
 }
 
